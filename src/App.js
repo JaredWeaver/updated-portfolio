96571604dd121bb7bd0be5e6d11c46860d00da95
JaredWeav <jared.weaver2@gmail.com>
2021-05-24 11:19:37 -0400

    added dark mode

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ThemeProvider } from 'styled-components';
+import { useDarkMode } from './components/UseDarkMode';
+import { GlobalStyles } from './components/GlobalStyles';
+import { lightTheme, darkTheme } from './components/Themes';
+import Toggle from './components/Toggle'
+
+const App = () => {
+  const [theme, themeToggler] = useDarkMode('light');
+  const themeMode = theme === 'light' ? lightTheme : darkTheme; 
+
+  return (
+    // <Router>
+    <ThemeProvider theme={themeMode}>
+      <>
+        <GlobalStyles />
+        <div>
+          <Toggle theme={theme} toggleTheme={themeToggler} />
+        </div>
+      </>
+    </ThemeProvider>
+
+    // </Router>
+  );
+};
+
+export default App;
---
+++ src/components/Themes/index.js
@@ -0,0 +1,12 @@
+export const lightTheme = {
+    body: '#e5dfd5',
+    text: '#363537',
+    toggleBorder: '#FFF',
+    background: '#F2EECB',
+}
+export const darkTheme = {
+    body: '#363537',
+    text: '#FAFAFA',
+    toggleBorder: '#6B8096',
+    background: '#606060',
+}
