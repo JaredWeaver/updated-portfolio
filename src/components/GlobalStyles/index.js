58ef35fc9004c489cf0124d1b9c9a476e63977e2
JaredWeav <jared.weaver2@gmail.com>
2021-05-26 12:46:29 -0400

    formatted intro

--- src/components/GlobalStyles/index.js
+++ src/components/GlobalStyles/index.js
@@ -7,15 +7,24 @@ export const GlobalStyles = createGlobalStyle`
     background: linear-gradient(0deg, #2f0909 0% , #393D47 94%) fixed no-repeat;
     font-family: 'Space Grotesk', sans-serif;
     margin: 0;
-    padding: 0;
+    padding: 0px 20px;
     color: white;
     text-shadow:
    -1px -1px 0 #000,  
     1px -1px 0 #000,
-  -1px 1px 0 #000,
+    -1px 1px 0 #000,
     1px 1px 0 #000;
     
   }
+
+  .Typewriter {
+    display: flex;
+    justify-content: center;
+    padding:4px;
+    text-shadow:
+   -1px -1px 0 #000,  
+    1px -1px 0 #000,
+    -1px 1px 0 #000,
+    1px 1px 0 #000;
+  }
   `;
-
-
--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,36 +1,66 @@
 import React from 'react';
 import styled from 'styled-components';
 import TypeWriter from 'typewriter-effect';
-// import Image from '../../assets/KikiJared.jpg';
+import FrontImage from '../../assets/kikijaredrock.jpg';
+import BackImage from '../../assets/walkaway.jpg';
+
+const TypewriteContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  flex-direction: row;
+  padding-top: 10%;
+  font-size: 3em;
+  padding-bottom: 0;
+  align-items: center;
+  margin: 4px;
+`;
+
+const TopIntroContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  flex-direction: row;
+  padding-top: 5%;
+  font-size: 3em;
+  padding-bottom: 0;
+  align-items: center;
+  margin: 4px;
+`;
 
 const IntroContainer = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
-  padding-top: 16%;
+  padding-top: 5%;
   font-size: 3em;
   padding-bottom: 0;
   align-items: center;
   text-align: center;
 `;
 
+const IntroPic = styled.img`
+  border-radius: 5%;
+  opacity: 0.8;
+  height: 8em;
+`;
+
 const IntroBioContainer = styled.section`
   display: flex;
-  margin-top: 1em;
+
   width: 66.6%;
-  background:transparent;
+  background: transparent;
   background-size: cover;
   overflow: hidden;
   border-radius: 5%;
-  p{
+  p {
     font-size: 1em;
+    text-align: center;
   }
 `;
 
 const Intro = () => {
   return (
     <>
-      <IntroContainer>
+      <TypewriteContainer>
         <TypeWriter
           onInit={(typewriter) => {
             typewriter
@@ -40,10 +70,22 @@ const Intro = () => {
               .start();
           }}
         />
+      </TypewriteContainer>
+
+      <TopIntroContainer>
+        <IntroPic src={FrontImage}></IntroPic>
         <IntroBioContainer>
-          <p> Developer that is addicted to learning best practices and new technologies to craft innovative user experiences.</p>
+          <p>
+            Seasoned Operations Manager turned Web Developer that is addicted to
+            learning best practices and new technologies to craft innovative
+            user experiences.
+          </p>
         </IntroBioContainer>
-      </IntroContainer>
+
+        <IntroPic src={BackImage}></IntroPic>
+      </TopIntroContainer>
+
+      <IntroContainer></IntroContainer>
     </>
   );
 };
